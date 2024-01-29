2631c4f5eea9865a7b2f4a552c017f068f251011
Sanskar Mahato <sanskarmahato@gmail.com>
2024-01-29 16:46:39 +0530

    created sign in api route (small change).

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const signup= async (req,res,next)=>{
      const {username,email,password}=req.body;
@@ -24,7 +25,7 @@ export const signin=async(req,res,next)=>{
       if(!validPassword)return next(errorHandler(401,'Wrong Credentials!'));
       const token=jwt.sign({id:validUser._id},process.env.JWT_SECRET)
       const {password:pass, ...rest}=validUser._doc;
-      res.cookie('access_token',token,{httpOnly:true}).status(200).json(validUser);
+      res.cookie('access_token',token,{httpOnly:true}).status(200).json(rest);
    } catch (error) {
      next(error); 
    }
